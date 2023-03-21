f9891c0da1ba952cd2d760b21adf3152efacbd14
RogerRocaVentura <3aroger.roca@gmail.com>
2023-03-21 20:13:11 +0100

    Update ChessDB.sql

--- ChessDB.sql
+++ ChessDB.sql
@@ -1,80 +1,42 @@
--- Create the database
-CREATE DATABASE ChessDB;
-
--- Use the database
-USE ChessDB;
-
--- Create the table for players
-CREATE TABLE Players (
-    id INT PRIMARY KEY AUTO_INCREMENT
-    name VARCHAR [255] NOT NULL
-    country VARCHAR [255]
-    rating INT
-    birthdate DATE
-    gender ENUM['M' , 'F']
+CREATE TABLE players (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
--- Create the table for tournaments
-CREATE TABLE Tournaments (
-    id INT PRIMARY KEY AUTO_INCREMENT
-    name VARCHAR[255] NOT NULL
-    location VARCHAR[255]
-    start_date DATE
-    end_date DATE
-    organizer VARCHAR[255]
+CREATE TABLE games (
+    id INT PRIMARY KEY,
+    white_player_id INT NOT NULL,
+    black_player_id INT NOT NULL,
+    result VARCHAR(10),
+    FOREIGN KEY (white_player_id) REFERENCES players(id),
+    FOREIGN KEY (black_player_id) REFERENCES players(id)
 );
 
--- Create the table for games
-CREATE TABLE Games (
-    id INT PRIMARY KEY AUTO_INCREMENT
-    tournament_id INT
-    white_player_id INT
-    black_player_id INT
-    result ENUM['1-0', '0-1', '1/2-1/2', '*']
-    moves TEXT
-    opening VARCHAR[255]
-    FOREIGN KEY [tournament_id] REFERENCES Tournaments[id]
-    FOREIGN KEY [white_player_id] REFERENCES Players[id]
-    FOREIGN KEY [black_player_id] REFERENCES Players[id]
+CREATE TABLE moves (
+    game_id INT NOT NULL,
+    move_number INT NOT NULL,
+    move_text VARCHAR(10) NOT NULL,
+    PRIMARY KEY (game_id, move_number),
+    FOREIGN KEY (game_id) REFERENCES games(id)
 );
 
--- Create the table for openings
-CREATE TABLE Openings (
-    id INT PRIMARY KEY AUTO_INCREMENT
-    name VARCHAR[255] NOT NULL
-    moves TEXT
-    eco_code VARCHAR[10]
-);
+INSERT INTO players (id, name) VALUES (1, 'Roger Roca');
+INSERT INTO players (id, name) VALUES (2, 'Magnus Carlsen');
 
--- Create the table for variations
-CREATE TABLE Variations (
-    id INT PRIMARY KEY AUTO_INCREMENT
-    name VARCHAR[255] NOT NULL
-    moves TEXT
-    opening_id INT
-    FOREIGN KEY [opening_id] REFERENCES Openings[d]
-);
+INSERT INTO games (id, white_player_id, black_player_id, result) VALUES (1, 1, 2, '1-0');
 
--- Create the table for tags
-CREATE TABLE Tags (
-    id INT PRIMARY KEY AUTO_INCREMENT
-    name VARCHAR[255] NOT NULL
-);
+INSERT INTO moves (game_id, move_number, move_text) VALUES (1, 1, 'e4');
+INSERT INTO moves (game_id, move_number, move_text) VALUES (1, 2, 'e5');
+INSERT INTO moves (game_id, move_number, move_text) VALUES (1, 3, 'Nf3');
+INSERT INTO moves (game_id, move_number, move_text) VALUES (1, 4, 'Nc6');
+INSERT INTO moves (game_id, move_number, move_text) VALUES (1, 5, 'Bb5');
+INSERT INTO moves (game_id, move_number, move_text) VALUES (1, 6, 'a6');
+INSERT INTO moves (game_id, move_number, move_text) VALUES (1, 7, 'Ba4');
+INSERT INTO moves (game_id, move_number, move_text) VALUES (1, 8, 'Nf6');
+INSERT INTO moves (game_id, move_number, move_text) VALUES (1, 9, 'O-O');
+INSERT INTO moves (game_id, move_number, move_text) VALUES (1, 10, 'Be7');
+INSERT INTO moves (game_id, move_number, move_text) VALUES (1, 11, 'Re1');
+INSERT INTO moves (game_id, move_number, move_text) VALUES (1, 12, 'b5');
+INSERT INTO moves (game_id, move_number, move_text) VALUES (1, 13, 'Bb3');
 
--- Create the table for game tags
-CREATE TABLE GameTags (
-    game_id INT,
-    tag_id INT,
-    PRIMARY KEY (game_id, tag_id),
-    FOREIGN KEY (game_id) REFERENCES Games(id),
-    FOREIGN KEY (tag_id) REFERENCES Tags(id)
-);
 
--- Create the table for opening tags
-CREATE TABLE OpeningTags (
-    opening_id INT,
-    tag_id INT,
-    PRIMARY KEY (opening_id, tag_id),
-    FOREIGN KEY (opening_id) REFERENCES Openings(id),
-    FOREIGN KEY (tag_id) REFERENCES Tags(id)
-);
