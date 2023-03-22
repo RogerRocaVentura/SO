b0d92c57bc50115d34df05f18277d76a5e2d5465
Roger RV <126819874+RogerRocaVentura@users.noreply.github.com>
2023-03-22 16:18:37 +0100

    Update Libros.c

--- 5.1/Libros.c
+++ 5.1/Libros.c
@@ -9,9 +9,9 @@ struct libro {
 
 int main() {
   struct libro libros[100];
-  // Ahora se puede acceder a cada campo de cada libro en el arreglo 'libros'
+  
   libros[0].precio = 20;
   libros[1].likes = 50;
-  // ... y así sucesivamente
+ 
   return 0;
 }
